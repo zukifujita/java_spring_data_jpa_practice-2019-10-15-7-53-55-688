089583b3904dbe8afda206b82290fc94be9a4bea
MIZUKI FUJITA (DOCU-IRIS-ISD-OOCLL/MNL) <FUJITMI@OOCL.COM>
2019-10-16 17:28:57 +0800

    [MIZUKI & ADRIAN] Added @Query and @Param in CompanyRepository.java

--- src/main/java/com/tw/apistackbase/repository/CompanyRepository.java
+++ src/main/java/com/tw/apistackbase/repository/CompanyRepository.java
@@ -3,10 +3,11 @@ package com.tw.apistackbase.repository;
 import com.tw.apistackbase.core.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
     @Query("SELECT c FROM Company c WHERE c.name = :name")
-    Company findOneByName(String name);
+    Company findOneByName(@Param("name") String name);
 }
