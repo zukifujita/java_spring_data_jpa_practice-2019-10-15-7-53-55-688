20febcd8f84843a408cef9410bbf779a7f755759
MIZUKI FUJITA (DOCU-IRIS-ISD-OOCLL/MNL) <FUJITMI@OOCL.COM>
2019-10-16 17:20:35 +0800

    [MIZUKI & ADRIAN] Added @Query in CompanyRepository

--- src/main/java/com/tw/apistackbase/repository/CompanyRepository.java
+++ src/main/java/com/tw/apistackbase/repository/CompanyRepository.java
@@ -2,8 +2,11 @@ package com.tw.apistackbase.repository;
 
 import com.tw.apistackbase.core.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+    @Query("SELECT c FROM Company c WHERE c.name = :name")
+    Company findOneByName(String name);
 }
