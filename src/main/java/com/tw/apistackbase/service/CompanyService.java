cf0e31ddd3ae9f60ea7d97e11f200398122bff29
MIZUKI FUJITA (DOCU-IRIS-ISD-OOCLL/MNL) <FUJITMI@OOCL.COM>
2019-10-17 18:40:08 +0800

    [MIZUKI & ADRIAN] Removed unnecessary imports

--- src/main/java/com/tw/apistackbase/service/CompanyService.java
+++ src/main/java/com/tw/apistackbase/service/CompanyService.java
@@ -4,8 +4,6 @@ import com.tw.apistackbase.core.Company;
 import com.tw.apistackbase.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
