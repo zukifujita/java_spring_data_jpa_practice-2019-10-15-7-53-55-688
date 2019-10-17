3cf759a9c6c2b782707d7182c8574d50c725bd41
MIZUKI FUJITA (DOCU-IRIS-ISD-OOCLL/MNL) <FUJITMI@OOCL.COM>
2019-10-17 15:08:26 +0800

    [MIZUKI & ADRIAN] Added LIKE Query for CompanyRepository

--- src/main/java/com/tw/apistackbase/repository/CompanyRepository.java
+++ src/main/java/com/tw/apistackbase/repository/CompanyRepository.java
@@ -6,8 +6,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
-    @Query("SELECT c FROM Company c WHERE c.name = :name")
-    Company findOneByName(@Param("name") String name);
+    Iterable<Company> findByNameContaining(@Param("name") String name);
 }
